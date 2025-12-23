78c4cf8ac74a8437454a95761ede4001f8ea4437
mukilan143 <mukilanmukilan174@gmail.com>
2025-12-23 16:10:47 +0530

    User_Report_Route_updated

--- src/Dashboard/userReport.js
+++ src/Dashboard/userReport.js
@@ -5,7 +5,7 @@ import { prisma } from '../config/db.js'
 
 /**
  * CREATE User Report
- * POST /api/dashboard/user-report         
+ * POST /api/dashboard/user-report
  * Body: { userId: String, reportData: Json }
  */
 
@@ -37,6 +37,35 @@ router.post('/api/dashboard/user-report/:userId', async (req, res) => {
     });
   } catch (error) {
     res.status(500).json({ message: 'Failed to create user report', error: error.message });
+  }
+})
+
+
+/**
+ * GET User Reports
+ * GET /api/dashboard/user-report/:userId
+ */
+router.get('/api/dashboard/user-report/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    if (!userId) {
+      return res.status(400).json({ message: 'User ID is required' });
+    }
+    const reports = await prisma.userReport.findMany({
+      where: { userId }
+    });
+
+    if (!reports) {
+      return res.status(404).json({ message: 'User reports not found' });
+    }
+    res.status(200).json({
+      success: true,
+      message: 'User reports fetched successfully',
+      data: reports
+    });
+  } catch (error) {
+
   }
 })
 
