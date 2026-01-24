5691b638274a135a4dd41d0041be02697feec7a6
mukilan143 <mukilanmukilan174@gmail.com>
2026-01-24 22:53:28 +0530

    Admin_and_Form_Api_updated

--- src/AdminRoutes/adminUser.js
+++ src/AdminRoutes/adminUser.js
@@ -454,23 +454,16 @@ router.post(
   [protect, trackActivity, admin],
   async (req, res) => {
     try {
-      const { name, email, password, role } = req.body;
+      const { name, email, password } = req.body;
 
       if (
         !name ||
         !email ||
-        !password ||
-        !role
+        !password
       ) {
         return res.status(400).json({
           success: false,
           message: "All fields are required",
-        });
-      }
-      if (role.toUpperCase() !== "ADMIN"){
-        return res.status(400).json({
-          success: false,
-          message: "Role must be ADMIN",
         });
       }
 
