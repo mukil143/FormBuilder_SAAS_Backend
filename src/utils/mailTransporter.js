e17b5308bc18828c6b8a534b8d0a26aa2aa75d96
mukilan143 <mukilanmukilan174@gmail.com>
2026-03-29 13:52:40 +0530

    feat: Configure mail transporter with secure settings for Gmail

--- src/utils/mailTransporter.js
+++ src/utils/mailTransporter.js
@@ -2,6 +2,8 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
